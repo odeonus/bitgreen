4e4a5f5cd6a3dfa8ec5ace852628a1d93d59e398
barrystyle <barrystyle@westnet.com.au>
2020-04-24 02:56:33 +0800

    trial fix for pos-stake undo data (aka. the konez method)

--- src/validation.cpp
+++ src/validation.cpp
@@ -1545,6 +1545,8 @@ DisconnectResult CChainState::DisconnectBlock(const CBlock& block, const CBlockI
         return DISCONNECT_FAILED;
     }
 
+    g_blockman.m_pos_index.erase(std::remove(g_blockman.m_pos_index.begin(), g_blockman.m_pos_index.end(), pindex->hashProofOfStake), g_blockman.m_pos_index.end());
+
     CBlockUndo blockUndo;
     if (!UndoReadFromDisk(blockUndo, pindex)) {
         error("DisconnectBlock(): failure reading undo data");
@@ -4181,8 +4183,16 @@ bool BlockManager::LoadBlockIndex(
     CBlockTreeDB& blocktree,
     std::set<CBlockIndex*, CBlockIndexWorkComparator>& block_index_candidates)
 {
-    if (!blocktree.LoadBlockIndexGuts(consensus_params, [this](const uint256& hash) EXCLUSIVE_LOCKS_REQUIRED(cs_main) { CBlockIndex* idx = this->InsertBlockIndex(hash); if (!hash.IsNull()) {this->InsertPoSIndex(idx->hashProofOfStake);} return idx; }))
-        return false;
+    if (!blocktree.LoadBlockIndexGuts(
+            consensus_params,
+            [this](const uint256 &hash) EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
+              CBlockIndex *idx = this->InsertBlockIndex(hash);
+              if (idx->nStatus & BLOCK_VALID_MASK && !hash.IsNull()) {
+                this->InsertPoSIndex(idx->hashProofOfStake);
+              }
+              return idx;
+            }))
+      return false;
 
     // Calculate nChainWork
     std::vector<std::pair<int, CBlockIndex*> > vSortedByHeight;
